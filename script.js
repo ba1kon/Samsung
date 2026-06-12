a0d2fa1fac12c639bb060d3b24bd6823d23801bc
student <student@skillset.edu.kz>
2026-06-12 18:00:23 +0500

    final project

--- script.js
+++ script.js
@@ -8,6 +8,17 @@ let findphn = document.querySelector("#findphn");
 let bestphn = document.querySelector("#bestphn");
 bestphn.addEventListener('click', function() {
     findphn.textContent = "Sumsung Galaxy S25 Ultra.";
-    findphn.style.color = "#001d6ba8";
+    findphn.style.color = "#b6bbffa8";
     findphn.style.fontWeight = "700";
 });
+ let form = document.querySelector("#contactsForm");
+ let formMessage = document.querySelector("#formMesssage");
+
+ form.addEventListener("submit", function(event) {
+    event.preventDefault()
+
+    formMessage.textContent = "Спасибо! Сообщение отправлено.";
+    formMessage.style.color = "green";
+    
+    form.requestFullscreen()
+ });
